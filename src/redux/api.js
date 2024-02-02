64659447e68414373eab652bdc86f1d066bc2f3f
sameedmohsin28 <sameedmohsin22@hotmail.com>
2024-02-02 19:08:16 +0500

    update

--- src/redux/api.js
+++ src/redux/api.js
@@ -4,9 +4,9 @@ import axios from 'axios';
 // const currencyCode = 'USD';
 
 export const fetchExchangeRates = createAsyncThunk('currency/fetchExchangeRates', async (code = 'USD') => {
-  const API_URL = `https://api.exchangerate.host/latest?base=${code}`;
+  const API_URL = `https://v6.exchangerate-api.com/v6/62265867183a1d6b9d189c99/latest/${code}`;
   const response = await axios.get(API_URL);
-  const rateResponse = response.data.rates;
+  const rateResponse = response.data.conversion_rates;
   const usdRates = Object.keys(rateResponse).map((eachRate) => (
     {
       symbol: eachRate,
